e3626bcd3321031525a804072dd8cbbbe79f0ba5
Gunayazhini Vijayakumar <au15ra06@gmail.com>
2026-09-12 17:16:07 +0530

    Add reusable card component

---
+++ Card.jsx
@@ -0,0 +1,8 @@
+export default function Card({ title, children, style }) {
+  return (
+    <div className="card" style={style}>
+      {title && <h3>{title}</h3>}
+      {children}
+    </div>
+  )
+}
