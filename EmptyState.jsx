7f82a48ebd70543e00ef28040608f3eb980572f5
Gunayazhini Vijayakumar <au15ra06@gmail.com>
2026-09-13 18:03:51 +0530

    Delete EmptyState.jsx

--- EmptyState.jsx
+++
@@ -1,7 +0,0 @@
-export default function EmptyState({ children }) {
-  return (
-    <div className="card" style={{ borderLeftColor: 'var(--line)' }}>
-      <p style={{ margin: 0 }}>{children}</p>
-    </div>
-  )
-}
